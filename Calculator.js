600ab987d419f327a0d3d728f2fa89b1d76b4737
Kevin Warner <Kevin Warner>
2017-01-24 16:24:57 -0700

    finished project

--- Calculator.js
+++ Calculator.js
@@ -1,7 +1,35 @@
-function message(number){
-	if (number === 1 || number === 2) {
-		document.getElementById('result').innerHTML = "You clicked button number " + number;
-	} else {
-		document.getElementById('result').innerHTML = "You clicked the wrong button";
-	}
+var previous = [];
+
+function input(input){
+	document.getElementById('result').value += input;
+	previous.push(document.getElementById('result').value)
+}
+
+function answer(input){
+	var equation = document.getElementById('result').value;
+	equation = eval(equation);
+	var ans = equation;
+	previous.push(ans);
+	document.getElementById('result').value = equation;
+	createHis();
+} 
+
+function createHis(){
+	var select = document.getElementById("previousHis");
+	for(var i = 0; i < previous.length; i++) {
+    var opt = previous[i];
+    var el = document.createElement("option");
+    el.textContent = opt;
+    el.value = opt;
+    select.appendChild(el);
+};
+}
+
+function copyHis(){
+	var text = document.getElementById('previousHis').value;
+	document.getElementById('result').value = text;
+}
+
+function clearHis(){
+	location.reload();
 }
