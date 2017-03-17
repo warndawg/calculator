45a15d4ec76726b3f90b49b41aa25b43e55c5808
Kevin Warner <Kevin Warner>
2017-03-16 18:42:20 -0600

    latest update

--- Calculator.js
+++ Calculator.js
@@ -2,7 +2,9 @@ var previous = [];
 
 function input(input){
 	document.getElementById('result').value += input;
-	previous.push(document.getElementById('result').value)
+	previous.push(document.getElementById('result').value);
+	checkDec();
+	 multiDivStart(input);
 }
 
 function answer(input){
@@ -17,12 +19,12 @@ function answer(input){
 function createHis(){
 	var select = document.getElementById("previousHis");
 	for(var i = 0; i < previous.length; i++) {
-    var opt = previous[i];
-    var el = document.createElement("option");
-    el.textContent = opt;
-    el.value = opt;
-    select.appendChild(el);
-};
+    	var opt = previous[i];
+    	var el = document.createElement("option");
+    	el.textContent = opt;
+    	el.value = opt;
+    	select.appendChild(el);
+	};
 }
 
 function copyHis(){
@@ -32,4 +34,25 @@ function copyHis(){
 
 function clearHis(){
 	location.reload();
+}
+
+function checkDec(){
+	var check = document.getElementById('result').value;
+	var lstTwo = check.substring(check.length - 2, check.length)
+	if (lstTwo === '..'){
+		alert('you can not put in two decmials in a row');
+		document.getElementById('result').value = '';
+		return 
+	}
+	else if (lstTwo === '--' || lstTwo === '++'){
+		var doubleNeg = document.getElementById('result').value;
+		document.getElementById('result').value = doubleNeg.replace(lstTwo, '+');
+	}
+}
+
+function multiDivStart(input){
+	var check = document.getElementById('result').value;
+	if (check === '*' || check === '/'){
+		document.getElementById('result').value = check.replace(input, '1'+input);
+	}
 }
